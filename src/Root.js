6160a0031ea6079f475dfbb9d331e518b31154bb
Daylan <daylanberry@gmail.com>
2020-08-27 15:51:38 -0700

    created middlewares

--- src/Root.js
+++ src/Root.js
@@ -2,13 +2,15 @@ import React from 'react'
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reducers from 'reducers';
+import async from 'middlewares/async';
+import stateValidater from 'middlewares/stateValidator'
 import reduxPromise from 'redux-promise';
 
 export default ({ children, initialState={} }) => {
   const store = createStore(
     reducers,
     initialState,
-    applyMiddleware(reduxPromise)
+    applyMiddleware(async, stateValidater)
   )
   return (
     <Provider store={store}>
--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -1,4 +1,18 @@
-export default = ({ dispatch }) => next => action => {
+export default ({ dispatch }) => next => action => {
+  // Check to see action has a promise on payload property
+
+  // If it does, wait for it to resolve
+
+  // If it doesn't, send the action on to next middleware
+
+  if (!action.payload || !action.payload.then) {
+    return next(action)
+  }
+
+  action.payload.then((response) => {
+    const newAction = { ...action, payload: response}
+    dispatch(newAction)
+  })
 
 }
 
